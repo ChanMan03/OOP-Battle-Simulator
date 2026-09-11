7c67213aed3d0c49d1b78ed4ae387681c1ae2a5d
ChanMan03 <chanmanmiller19@gmail.com>
2026-09-11 12:50:35 -0500

    Update game.py

--- game.py
+++ game.py
@@ -10,9 +10,14 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Ethan")
+    goblin = Goblin("New Century")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblinTwo = Goblin("Gribble")
+
+    print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
